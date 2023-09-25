26e8f4d730b7bcf35260665c4e65dcb28596a23f
chmandzhiev <chmandzhiev@gmail.com>
2023-09-25 12:31:28 +0300

    Beginner Series #3 Sum of Numbers

--- 11.py
+++ 11.py
@@ -1,1 +1,15 @@
-test
+# Beginner Series #3 Sum of Numbers
+
+def get_sum(a,b):
+    s = 0
+    if a != b:
+        if a < b: 
+            for i in range(a,b+1):
+                s += i
+        if a > b:
+            for i in range(a,b-1,-1):
+                s += i
+        return s
+    else:
+        return a
+
